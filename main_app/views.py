f8642ddcdb5ff6a801e2a88db1dc1db4fac2dbce
b-delacruz <brianmdelacruz@gmail.com>
2022-08-08 12:20:57 -0400

    add toys view with full CRUD

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Finch
+from .models import Finch, Toy
 from .forms import FeedingForm
+from django.views.generic import ListView, DetailView
+
+class ToyUpdate(UpdateView):
+  model = Toy
+  fields = ['name', 'color']
+
+class ToyDelete(DeleteView):
+  model = Toy
+  success_url = '/toys/'
+
+class ToyList(ListView):
+  model = Toy
+
+class ToyDetail(DetailView):
+  model = Toy
+
+class ToyCreate(CreateView):
+  model = Toy
+  fields = '__all__'
 
 class FinchCreate(CreateView):
   model = Finch
