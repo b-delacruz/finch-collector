efd63b7e74891e5cb3ca145a13d0b5e3294437aa
b-delacruz <brianmdelacruz@gmail.com>
2022-08-08 09:51:48 -0400

    add a finch update

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
 from .models import Finch
+
+class FinchCreate(CreateView):
+  model = Finch
+  fields = '__all__'
 
 
 # Define the home view
@@ -12,7 +17,7 @@ def about(request):
 def finches_index(request):
   finches = Finch.objects.all()
   return render(request, 'finches/index.html', { 'finches': finches })
-  
+
 def finches_detail(request, finch_id):
   finch = Finch.objects.get(id=finch_id)
   return render(request, 'finches/detail.html', { 'finch': finch })
