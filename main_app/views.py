6a32f753a1211bcd54f52594fc8885bdc765b667
b-delacruz <brianmdelacruz@gmail.com>
2022-08-08 07:50:37 -0400

    add all finches to base block

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+class Finch:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+finches = [
+  Finch('Lolo', 3),
+  Finch('Sachi', 0),
+  Finch('Fancy', 4),
+  Finch('Bonk', 6)
+]
+
 # Define the home view
 def home(request):
   return HttpResponse('<h1>Hello Buttercup ᓚᘏᗢ</h1>')
 def about(request):
   return render(request, 'about.html')
+def finches_index(request):
+  return render(request, 'finches/index.html', { 'finches': finches })
