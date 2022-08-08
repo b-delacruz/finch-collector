b9643bed244bd5ba8f4a7cbd1ea11c52bd2037b1
b-delacruz <brianmdelacruz@gmail.com>
2022-08-08 11:02:07 -0400

    update feeding view

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.urls import reverse
 
+# A tuple of 2-tuples
+MEALS = (
+  ('B', 'Breakfast'),
+  ('L', 'Lunch'),
+  ('D', 'Dinner')
+)
+
 class Finch(models.Model):
   name = models.CharField(max_length=100)
   age = models.IntegerField()
@@ -10,3 +17,16 @@ class Finch(models.Model):
 
   def get_absolute_url(self):
     return reverse('finches_detail', kwargs={'finch_id': self.id})
+
+
+class Feeding(models.Model):
+  date = models.DateField('Feeding date')
+  meal = models.CharField(
+    max_length=1,
+    choices=MEALS,
+    default=MEALS[0][0]
+  )
+  finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f"{self.get_meal_display()} on {self.date}"
